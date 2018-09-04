44846e411b2a2d55e870cb980b3c18a44be3e353
Ivan Ko <kostivan+git@gmail.com>
2018-09-04 15:17:43 -0700

    Added AND.java

---
+++ Sim1_AND.java
@@ -0,0 +1,23 @@
+/* Simulates a physical AND gate.
+ *
+ * Author: TODO
+ */
+
+public class Sim1_AND
+{
+	public void execute()
+	{
+		// TODO: fill this in!
+	}
+
+
+
+	public RussWire a,b;   // inputs
+	public RussWire out;   // output
+
+	public Sim1_AND()
+	{
+		// TODO: fill this in!
+	}
+}
+
